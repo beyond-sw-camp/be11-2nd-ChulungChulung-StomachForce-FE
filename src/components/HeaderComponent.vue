553a9a7cc1e01a9df8b825e7f3f0038144d7dbb9
dksqjq4851 <dksqjq4851@naver.com>
2025-02-13 18:19:18 +0900

    기본홈화면프로토타입

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-app-bar app color="white" flat class="top-bar">
+      <v-container>
+        <v-row align="center">
+          <!-- 상단: 로고 & 검색창 & 로그인 버튼 -->
+          <v-col class="d-flex align-center">
+            <v-img src="/stomach.png" contain max-height="60" max-width="60" class="mr-2"></v-img>
+            <v-btn text class="text-h5 font-weight-bold logo-text" to="/">뱃살력</v-btn>
+            <v-text-field
+              class="ml-4 search-box rounded-search"
+              solo
+              placeholder="검색"
+              append-icon="mdi-magnify"
+            ></v-text-field>
+          </v-col>
+          <v-col class="d-flex justify-end">
+            <v-btn v-if="!isLogin" text to="/signup">회원가입</v-btn>
+            <v-img v-if="!isLogin" text @click="doLogin" src="/login.png" contain max-height="40" max-width="80" class="login-img"></v-img>
+            <v-btn v-if="isLogin" text @click="doLogout">로그아웃</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    
+    <!-- 별도의 줄: 메뉴 항목 -->
+    <v-app-bar color="lightgrey" flat class="menu-bar">
+      <v-container>
+        <v-row justify="center">
+          <v-btn text to="/notice" class="menu-item">공지사항</v-btn>
+          <v-btn text to="/restaurant" class="menu-item">레스토랑</v-btn>
+          <v-btn text to="/influencer" class="menu-item">인플루언서</v-btn>
+          <v-btn text to="/event" class="menu-item">이벤트</v-btn>
+          <v-btn text to="/customer" class="menu-item">고객센터</v-btn>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isLogin: false
+      };
+    },
+    created() {
+      const token = localStorage.getItem('token');
+      if (token) {
+        this.isLogin = true;
+      }
+    },
+    methods: {
+      doLogout() {
+        localStorage.clear();
+        window.location.href = "/";
+      },
+      doLogin(){
+        window.location.href = "/user/login";
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .top-bar {
+    border-bottom: 2px solid black;
+  }
+  .menu-bar {
+    border-bottom: 2px solid lightgray;
+  }
+  .menu-item {
+    font-size: 18px;
+    font-weight: 600;
+    color: black;
+  }
+  .search-box {
+    max-width: 250px;
+  }
+  .rounded-search {
+    border-radius: 20px;
+    border: 1px solid black;
+  }
+  .logo-text {
+    color: #F04E23;
+  }
+  .login-img {
+  cursor: pointer;
+}
+  </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,48 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import vuetify from './plugins/vuetify'
+import router from './router'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token');
+        if(token){
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config;
+    },
+    error => {
+        // 에러가 발생시, 사용자의 본래요청의 화면으로 되돌아가야함
+        return Promise.reject(error)
+    }
+)
+axios.interceptors.response.use(
+    response => response,
+    async error => {
+        if(error.response && error.response.status === 401){
+            try{
+                const refreshToken = localStorage.getItem('refreshToken');
+            localStorage.removeItem("token");
+            const response = await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member/refresh-token`, {refreshToken});
+            const token = response.data.token;
+            localStorage.setItem("token", token)
+            window.location.reload();
+            }catch(e){
+                localStorage.clear();
+                window.location.href="/member/login"
+            }
+
+        }
+        return Promise.reject(error);
+    }
+)
+
+
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
+
